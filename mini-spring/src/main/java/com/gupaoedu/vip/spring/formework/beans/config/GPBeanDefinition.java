fbd3473ec5d83ebfe450bd20a21f58726be2c8a1
hufan30 <hufan30@163.com>
2021-03-23 17:16:06 +0800

    Update GPBeanDefinition.java

--- mini-spring/src/main/java/com/gupaoedu/vip/spring/formework/beans/config/GPBeanDefinition.java
+++ mini-spring/src/main/java/com/gupaoedu/vip/spring/formework/beans/config/GPBeanDefinition.java
@@ -5,33 +5,40 @@ import lombok.Data;
 /**
  * Created by Tom.
  */
+@Data
 public class GPBeanDefinition {
+    /**
+     * bean的全类名
+     */
     private String beanClassName;
     private boolean lazyInit = false;
+    /**
+     * bean在map中存储的key名称
+     */
     private String factoryBeanName;
     private boolean isSingleton = true;
 
-    public String getBeanClassName() {
-        return beanClassName;
-    }
-
-    public void setBeanClassName(String beanClassName) {
-        this.beanClassName = beanClassName;
-    }
-
-    public boolean isLazyInit() {
-        return lazyInit;
-    }
-
-    public void setLazyInit(boolean lazyInit) {
-        this.lazyInit = lazyInit;
-    }
-
-    public String getFactoryBeanName() {
-        return factoryBeanName;
-    }
-
-    public void setFactoryBeanName(String factoryBeanName) {
-        this.factoryBeanName = factoryBeanName;
-    }
+//    public String getBeanClassName() {
+//        return beanClassName;
+//    }
+//
+//    public void setBeanClassName(String beanClassName) {
+//        this.beanClassName = beanClassName;
+//    }
+//
+//    public boolean isLazyInit() {
+//        return lazyInit;
+//    }
+//
+//    public void setLazyInit(boolean lazyInit) {
+//        this.lazyInit = lazyInit;
+//    }
+//
+//    public String getFactoryBeanName() {
+//        return factoryBeanName;
+//    }
+//
+//    public void setFactoryBeanName(String factoryBeanName) {
+//        this.factoryBeanName = factoryBeanName;
+//    }
 }
