bcb3103b4e49b4627617f43f789805ec802004c7
hufan30 <hufan30@163.com>
2021-04-21 16:18:52 +0800

    更新handlerAdapter机制

--- mini-spring/src/main/java/com/gupaoedu/vip/spring/formework/webmvc/servlet/GPDispatcherServlet.java
+++ mini-spring/src/main/java/com/gupaoedu/vip/spring/formework/webmvc/servlet/GPDispatcherServlet.java
@@ -10,17 +10,15 @@ import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.regex.Pattern;
 
 @Slf4j
 public class GPDispatcherServlet extends HttpServlet {
 
     private final String CONTEXT_CONFIG_LOCATION = "contextConfigLocation";
-    private List<GPHandlerMapping> handlerMappings = new ArrayList<GPHandlerMapping>();
+    private List<GPHandlerMapping> handlerMappings = new ArrayList<>();
+    private List<GPHandlerAdapter> handlerAdapters = new ArrayList();
 
     private GPApplicationContext context;
 
@@ -46,6 +44,8 @@ public class GPDispatcherServlet extends HttpServlet {
 
         //handlerMapping，必须实现
         initHandlerMappings(context);
+        //初始化参数适配器，必须实现
+        initHandlerAdapters(context);
     }
 
     private void initHandlerMappings(GPApplicationContext context) {
@@ -59,7 +59,7 @@ public class GPDispatcherServlet extends HttpServlet {
                 Object controllerBean = context.getBean(beanDefinitonMapName);
                 Class<?> beanClass = controllerBean.getClass();
 
-                if(!beanClass.isAnnotationPresent(GPController.class)){
+                if (!beanClass.isAnnotationPresent(GPController.class)) {
                     continue;
                 }
                 //拿到这个Handler的baseurl
@@ -70,11 +70,11 @@ public class GPDispatcherServlet extends HttpServlet {
                 //拿到具体方法的url;
                 Method[] methods = beanClass.getMethods();
                 for (Method method : methods) {
-                    if(!method.isAnnotationPresent(GPRequestMapping.class)){
+                    if (!method.isAnnotationPresent(GPRequestMapping.class)) {
                         continue;
                     }
                     String methodUrl = method.getAnnotation(GPRequestMapping.class).value();
-                    String regex = ("/" + baseUrl + "/" + methodUrl.replaceAll("\\*",".*")).replaceAll("/+", "/");
+                    String regex = ("/" + baseUrl + "/" + methodUrl.replaceAll("\\*", ".*")).replaceAll("/+", "/");
                     Pattern pattern = Pattern.compile(regex);
                     this.handlerMappings.add(new GPHandlerMapping(pattern, controllerBean, method));
                 }
@@ -83,6 +83,17 @@ public class GPDispatcherServlet extends HttpServlet {
             log.error(e.getMessage(), e);
         }
 
+    }
+
+    /**
+     * 把一个requet请求变成一个handler，参数都是字符串的，自动配到handler中的形参
+     * 可想而知，他要拿到HandlerMapping才能干活
+     * 就意味着，有几个HandlerMapping就有几个HandlerAdapter
+     *
+     * @param context
+     */
+    private void initHandlerAdapters(GPApplicationContext context) {
+        this.handlerAdapters.add(new GPHandlerAdapter());
     }
 
     private void initThemeResolver(GPApplicationContext context) {
