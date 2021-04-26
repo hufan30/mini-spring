6399d695119f17988450a9c093859af4f3234437
hufan30 <hufan30@163.com>
2021-04-26 08:56:09 +0800

    Update GPHandlerAdapter.java

--- mini-spring/src/main/java/com/gupaoedu/vip/spring/formework/webmvc/servlet/GPHandlerAdapter.java
+++ mini-spring/src/main/java/com/gupaoedu/vip/spring/formework/webmvc/servlet/GPHandlerAdapter.java
@@ -11,6 +11,10 @@ import java.util.Map;
 
 /**
  * Created by Tom on 2019/4/13.
+ * 那真的感觉这个handler就是一个纯的适配器，字面上的意思
+ * 核心方法就是handler方法，无成员变量，就是一个处理逻辑；
+ * 根据传入参数的Request，response，handlerMapping，将它们整合处理
+ * 关键还是用handlerMapping来处理Request和response
  */
 public class GPHandlerAdapter {
 
@@ -19,6 +23,14 @@ public class GPHandlerAdapter {
     }
 
 
+    /**
+     * 核心处理逻辑，下面来体会一下，为什么需要一个专门的逻辑方法来处理；
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
     GPModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         return null;
     }
