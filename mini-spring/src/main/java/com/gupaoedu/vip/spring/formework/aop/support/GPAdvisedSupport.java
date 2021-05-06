a5c8f37a5d87c8e91da6e32d2044c7aaaf8898a7
hufan30 <hufan30@163.com>
2021-05-06 14:22:59 +0800

    Update GPAdvisedSupport.java

--- mini-spring/src/main/java/com/gupaoedu/vip/spring/formework/aop/support/GPAdvisedSupport.java
+++ mini-spring/src/main/java/com/gupaoedu/vip/spring/formework/aop/support/GPAdvisedSupport.java
@@ -32,46 +32,60 @@ public class GPAdvisedSupport {
         this.target = target;
     }
 
-    public Object getTarget(){
+    public Object getTarget() {
         return this.target;
     }
 
-    public Class<?> getTargetClass(){
+    public Class<?> getTargetClass() {
         return this.targetClass;
     }
 
+    /**
+     * 这个其实相当于是GPAdvisedSupport的初始化函数
+     * 调用了parse,里面会向methodCache存放缓存
+     *
+     * @param targetClass
+     */
     public void setTargetClass(Class<?> targetClass) {
         this.targetClass = targetClass;
         parse();
     }
 
-    public List<Object> getInterceptorsAndDynamicInterceptionAdvice(Method method, Class<?> targetClass) throws Exception{
-        List<Object> cached = methodCache.get(method);
-        if(cached == null){
-            Method m = targetClass.getMethod(method.getName(),method.getParameterTypes());
-
-            cached = methodCache.get(m);
-
-            //底层逻辑，对代理方法进行一个兼容处理
-            this.methodCache.put(m,cached);
-        }
-
-        return cached;
-    }
-
     private void parse() {
         String pointCut = config.getPointCut()
-                .replaceAll("\\.","\\\\.")
-                .replaceAll("\\\\.\\*",".*")
-                .replaceAll("\\(","\\\\(")
-                .replaceAll("\\)","\\\\)");
+                .replaceAll("\\.", "\\\\.")
+                .replaceAll("\\\\.\\*", ".*")
+                .replaceAll("\\(", "\\\\(")
+                .replaceAll("\\)", "\\\\)");
         //pointCut=public .* com.gupaoedu.vip.spring.demo.service..*Service..*(.*)
         //玩正则
-        String pointCutForClassRegex = pointCut.substring(0,pointCut.lastIndexOf("\\(") - 4);
+        String pointCutForClassRegex = pointCut.substring(0, pointCut.lastIndexOf("\\(") - 4);
         pointCutClassPattern = Pattern.compile("class " + pointCutForClassRegex.substring(
                 pointCutForClassRegex.lastIndexOf(" ") + 1));
 
 
+    }
+
+    /**
+     * 本质是从缓存中拿东西，感觉逻辑有点问题，一定要从缓存中拿
+     * 先根据传入参数method从缓存中拿，传参拿不到，就使用本地的targetClass的method来拿
+     * 初步推测是因为parse()方法有放入cache过，所以一定能拿到；
+     *
+     * @param method
+     * @return
+     * @throws Exception
+     */
+    public List<Object> getInterceptorsAndDynamicInterceptionAdvice(Method method) throws Exception {
+        List<Object> result = methodCache.get(method);
+        if (null == result) {
+            Method targetClassMethod = targetClass.getMethod(method.getName(), method.getParameterTypes());
+            /**
+             * 这里应该是parse()中有根据targetClass存入缓存中，所以默认一定能拿到
+             */
+            result = methodCache.get(targetClassMethod);
+            this.methodCache.put(method, result);
+        }
+        return result;
     }
 
     public boolean pointCutMatch() {
