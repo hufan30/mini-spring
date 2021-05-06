36d9ba4599788e66cf34c7afed46f2ca5e428771
hufan30 <hufan30@163.com>
2021-05-06 16:15:33 +0800

    Update GPJdkDynamicAopProxy.java

--- mini-spring/src/main/java/com/gupaoedu/vip/spring/formework/aop/GPJdkDynamicAopProxy.java
+++ mini-spring/src/main/java/com/gupaoedu/vip/spring/formework/aop/GPJdkDynamicAopProxy.java
@@ -29,9 +29,23 @@ public class GPJdkDynamicAopProxy implements GPAopProxy, InvocationHandler {
         return Proxy.newProxyInstance(classLoader, this.advised.getTargetClass().getInterfaces(), this);
     }
 
+    /**
+     * 执行代理的关键入口
+     * @param proxy
+     * @param method
+     * @param args
+     * @return
+     * @throws Throwable
+     */
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        /**
+         * 将每一个JoinPoint也就是被代理的业务方法（method）封装成一个拦截器，组合成一个拦截器链
+         */
         List<Object> interceptorsAndDynamicMethodMatchers = this.advised.getInterceptorsAndDynamicInterceptionAdvice(method);
+        /**
+         * 交给拦截器链MethodInvocation的proceed()方法执行
+         */
         GPMethodInvocation invocation = new GPMethodInvocation(proxy, this.advised.getTarget(), method, args, this.advised.getTargetClass(), interceptorsAndDynamicMethodMatchers);
         return invocation.proceed();
     }
