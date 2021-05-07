3457741c8ffbe373efee4dfb9ada54b9245ce009
hufan30 <hufan30@163.com>
2021-05-07 11:38:41 +0800

    Update GPAbstractAspectAdvice.java

--- mini-spring/src/main/java/com/gupaoedu/vip/spring/formework/aop/aspect/GPAbstractAspectAdvice.java
+++ mini-spring/src/main/java/com/gupaoedu/vip/spring/formework/aop/aspect/GPAbstractAspectAdvice.java
@@ -18,7 +18,22 @@ public abstract class GPAbstractAspectAdvice implements GPAdvice {
     }
 
     public Object invokeAdviceMethod(GPJoinPoint joinPoint, Object returnValue, Throwable tx) throws Throwable {
-        return null;
+        Class<?>[] paramTypes = this.aspectMethod.getParameterTypes();
+        if (null == paramTypes || paramTypes.length == 0) {
+            return this.aspectMethod.invoke(aspectTarget);
+        } else {
+            Object[] args = new Object[paramTypes.length];
+            for (int i = 0; i < paramTypes.length; i++) {
+                if (paramTypes[i] == GPJoinPoint.class) {
+                    args[i] = joinPoint;
+                } else if (paramTypes[i] == Throwable.class) {
+                    args[i] = tx;
+                } else if (paramTypes[i] == Object.class) {
+                    args[i] = returnValue;
+                }
+            }
+            return this.aspectMethod.invoke(aspectTarget, args);
+        }
     }
 
 
